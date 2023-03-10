9f1bdb6f5c4982e1b9b1c74cc779b90b0505c6de
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-10 23:59:25 +0300

    v02 +

--- src/Generics/interfacetype/GenericInterfaceStringImpl.java
+++ src/Generics/interfacetype/GenericInterfaceStringImpl.java
@@ -1,5 +1,5 @@
 package Generics.interfacetype;
-
+//ÖNEMLİ!!!!:Generics primitive data type ile çalışmaz.
 public class GenericInterfaceStringImpl implements GenericInterface<String>{
     @Override
     public void printValue(String type) {
