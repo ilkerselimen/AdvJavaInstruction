64a6e170b7ff35c03d6e319e3d00afc9e565cff5
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-10 22:00:38 +0300

    Initial commit

---
+++ src/Generics/classes/NonGenericClass.java
@@ -0,0 +1,16 @@
+package Generics.classes;
+
+public class NonGenericClass {
+
+    //field(özellik):farklı data type larını referans alsın
+
+    private Object object;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
---
+++ src/Generics/classes/RunnerGeneric.java
@@ -0,0 +1,12 @@
+package Generics.classes;
+
+public class RunnerGeneric {
+    public static void main(String[] args) {
+        //Generics ile hangi data type ını kullanacağımıza karar verebiliriz.
+        //Java 7 ile birlikte sağ tarafta data typeını yazmaya gerek kalmadı.
+        GenericClass<String> object1=new GenericClass<>();
+        object1.
+
+
+    }
+}
