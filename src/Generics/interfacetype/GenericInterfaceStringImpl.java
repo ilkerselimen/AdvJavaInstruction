1b2cfeea666c6720aa1e5072f7c5f3da11aa358f
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-10 23:46:01 +0300

    v02

---
+++ src/Generics/interfacetype/GenericInterfaceStringImpl.java
@@ -0,0 +1,13 @@
+package Generics.interfacetype;
+
+public class GenericInterfaceStringImpl implements GenericInterface<String>{
+    @Override
+    public void printValue(String type) {
+
+    }
+
+    @Override
+    public String getValue() {
+        return null;
+    }
+}
