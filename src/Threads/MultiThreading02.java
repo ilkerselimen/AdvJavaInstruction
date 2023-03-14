31528c454a9b8c22fac69ef1dd5ed5ef3cae5b79
İlker Şelimen <ilkerselimen@gmail.com>
2023-03-14 22:25:10 +0300

    v05

--- src/Threads/MultiThreading02.java
+++ src/Threads/MultiThreading02.java
@@ -1,5 +1,8 @@
 package Threads;
 //threadler aynı anda aynı kaynağa erişip değişiklik yapmak isterlerse ne olacak?
+//multithreading programlamada birden fazla thread aynı kaynağa(critical section) aynı anda erişip değişiklik yapmaya çalışırsa
+//istenmeyen sonuçlar olur. Bu durumda threadlere sırayla erişim verilir. Yani ortak kaynağı bir thread kullanırken diğer
+//thread beklemelidir. Bu işlem syncronized keywordü ile gerçekleştirlir.
 public class MultiThreading02 {
 
     public static int counter=0;
@@ -36,7 +39,9 @@ public class MultiThreading02 {
 
 class Counter{
     //counter değişkeninin değerini 1000 kez arttırsın.
-    public static void count(){
+    public static synchronized void count(){
+        //synchronized ile metoda erişen threadleri sıraya koymuş oluruz.
+        //yani metodu aynı anda sadece bir thread kullanabilsin
         for (int i = 1; i <1001 ; i++) {
             MultiThreading02.counter++;//1000+1000= beklenen 2000
         }
